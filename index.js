ace9aab09aa596743ca537da3f9b85caf5b395de
piarda <matt.rindini@gmail.com>
2025-07-09 15:06:48 -0400

    Created and returned the 5 functions

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+function findMaximum(num1,num2) {
+    return Math.max(num1, num2);
+}
 
+function isPalindrome(word) {
+    let backward = word.split("").reverse().join("");
+    if (backward == word) {
+        return true
+    }
+    return false
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = (originalPrice * discountPercentage) / 100;
+  return originalPrice - discountAmount;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
